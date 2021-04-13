b5654f31ed0f4961815dad59a13e84dc41429d85
Felipe Tiefaldi <felipe_tiefaldi@outlook.com>
2021-04-13 19:12:06 -0300

    Update ubunto_infrastructure_packages.sh

--- ubunto_infrastructure_packages.sh
+++ ubunto_infrastructure_packages.sh
@@ -11,33 +11,22 @@ echo INFRASTRUCTURE PACKAGE / NETWORKS
 	apt-get install wget -y
 	apt-get install curl -y
 	apt-get install vim -y
+	apt-get alien -y
 	apt-get install libaio1 libaio-dev -y
 
 
 echo INFRASTRUCTURE PACKAGE / SQLPLUS
 
-	
 	mkdir -p /tmp/oracle
 	cd /tmp/oracle
-	wget https://download.oracle.com/otn_software/linux/instantclient/211000/instantclient-basic-linux.x64-21.1.0.0.0.zip
-	wget https://download.oracle.com/otn_software/linux/instantclient/211000/instantclient-sqlplus-linux.x64-21.1.0.0.0.zip
-	unzip -o instantclient-basic-linux.x64-21.1.0.0.0.zip
-	unzip -o instantclient-sqlplus-linux.x64-21.1.0.0.0.zip
-	echo "alias sqlplus='cd /tmp/oracle/instantclient_21_1 && ./sqlplus' " >> ~/.bashrc
-	alias sqlplus='cd /tmp/oracle/instantclient_21_1 && ./sqlplus'
-	#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME/lib:$ORACLE_HOME
-	chmod -R 755 /tmp/oracle
-	ldconfig
-
-
-#IF YOU WANT TO ADD IN THE VARIABLES
-
-	# export LD_LIBRARY_PATH=/opt/oracle/instantclient_21_1/
-	# ORACLE_HOME=/opt/oracle/instantclient_21_1/
-	# PATH=$ORACLE_HOME/bin:$PATH
-	# LD_LIBRARY_PATH=$ORACLE_HOME/lib
-	# export ORACLE_HOME
-	# export LD_LIBRARY_PATH
-	# export PATH
+	wget https://download.oracle.com/otn_software/linux/instantclient/211000/oracle-instantclient-basic-21.1.0.0.0-1.x86_64.rpm
+   	wget https://download.oracle.com/otn_software/linux/instantclient/211000/oracle-instantclient-sqlplus-21.1.0.0.0-1.x86_64.rpm
+    	wget https://download.oracle.com/otn_software/linux/instantclient/211000/oracle-instantclient-devel-21.1.0.0.0-1.x86_64.rpm
+    	alien -i oracle-instantclient-basic-21.1.0.0.0-1.x86_64.rpm
+    	alien -i oracle-instantclient-sqlplus-21.1.0.0.0-1.x86_64.rpm
+    	alien -i oracle-instantclient-devel-21.1.0.0.0-1.x86_64.rpm
+    	echo /usr/lib/oracle/21/client64 >> /etc/ld.so.conf.d/oracle.conf
+    	ldconfig
+	
 
 echo FINISHED INSTALLATIONS...
