8f271280467f6ee9a4247621f5a3bbfdb773ce67
Felipe Tiefaldi <felipe_tiefaldi@outlook.com>
2021-04-13 17:49:06 -0300

    Update ubunto_infrastructure_packages.sh

--- ubunto_infrastructure_packages.sh
+++ ubunto_infrastructure_packages.sh
@@ -23,8 +23,9 @@ echo INFRASTRUCTURE PACKAGE / SQLPLUS
 	wget https://download.oracle.com/otn_software/linux/instantclient/211000/instantclient-sqlplus-linux.x64-21.1.0.0.0.zip
 	unzip -o instantclient-basic-linux.x64-21.1.0.0.0.zip
 	unzip -o instantclient-sqlplus-linux.x64-21.1.0.0.0.zip
-	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME/lib:$ORACLE_HOME
+	echo alias sqlplus='cd /tmp/oracle/instantclient_21_1 && ./sqlplus' >> ~/.bashrc
 	alias sqlplus='cd /tmp/oracle/instantclient_21_1 && ./sqlplus'
+	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME/lib:$ORACLE_HOME
 
 
 #IF YOU WANT TO ADD IN THE VARIABLES
