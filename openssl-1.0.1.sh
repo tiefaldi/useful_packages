01c5d66db1e235242f3b77be1ba59c322bdfb27a
Felipe Tiefaldi <felipe_tiefaldi@outlook.com>
2024-07-05 13:33:42 -0300

    Update openssl-1.0.1.sh

--- openssl-1.0.1.sh
+++ openssl-1.0.1.sh
@@ -6,18 +6,18 @@ sudo apt-get update
 
 # Install necessary dependencies
 echo "Installing necessary dependencies..."
-sudo apt-get install -y build-essential checkinstall zlib1g-dev
+sudo apt-get install -y build-essential checkinstall zlib1g-dev wget
 
 # Download OpenSSL 1.0.1
 echo "Downloading OpenSSL 1.0.1..."
-wget https://www.openssl.org/source/old/1.0.1/openssl-1.0.1.tar.gz
+wget https://www.openssl.org/source/old/1.0.1/openssl-1.0.1u.tar.gz
 
 # Extract the downloaded file
 echo "Extracting OpenSSL 1.0.1..."
-tar -xvzf openssl-1.0.1.tar.gz
+tar -xvzf openssl-1.0.1u.tar.gz
 
 # Navigate to the extracted directory
-cd openssl-1.0.1
+cd openssl-1.0.1u
 
 # Configure the installation
 echo "Configuring the installation..."
@@ -30,6 +30,11 @@ make
 # Install OpenSSL
 echo "Installing OpenSSL..."
 sudo make install
+
+# Create symbolic links for libssl and libcrypto
+echo "Creating symbolic links for libssl and libcrypto..."
+sudo ln -sf /usr/local/ssl/lib/libssl.so.1.0.1 /usr/lib/libssl.so.1.0.1
+sudo ln -sf /usr/local/ssl/lib/libcrypto.so.1.0.1 /usr/lib/libcrypto.so.1.0.1
 
 # Verify the installation
 echo "Verifying the installation..."
